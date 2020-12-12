915fb738929c686f6e80ba61a242c75de134ed92
Long <hoanglonghaui97@gmail.com>
2020-12-12 21:42:45 +0700

    done for android

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,6 @@ module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
     'babel-plugin-transform-typescript-metadata',
-    'react-native-reanimated/plugin',
     ['@babel/plugin-proposal-decorators', { legacy: true }],
     [
       'module-resolver',
